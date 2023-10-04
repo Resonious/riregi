75333acd5f4badc6c443c98a32b041b7621d745b
Nigel Baillie <nigel@baillie.dev>
2023-10-04 18:20:29 +0900

    make orders page look better but probably perform horribly

--- lib/orders.dart
+++ lib/orders.dart
@@ -3,11 +3,16 @@ import 'package:flutter/material.dart';
 import 'data.dart';
 import 'regi.dart';
 
-class OrdersPage extends StatelessWidget {
+class OrdersPage extends StatefulWidget {
   const OrdersPage({super.key, required this.app});
 
   final ActiveAppState app;
 
+  @override
+  State<OrdersPage> createState() => _OrdersPageState();
+}
+
+class _OrdersPageState extends State<OrdersPage> {
   String formatTimestamp(int timestamp) {
     final datetime = DateTime.fromMillisecondsSinceEpoch(timestamp).toLocal();
     final y = datetime.year;
@@ -30,57 +35,123 @@ class OrdersPage extends StatelessWidget {
     return '$y-$m-$d ($wk) $hr:$min';
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final a = app;
+  String justTheDate(DateTime datetime) {
+    final y = datetime.year;
+    final m = datetime.month;
+    final d = datetime.day;
 
-    final ordersLen = a.rrOrdersLen(a.ctx);
+    return '$y-$m-$d';
+  }
 
+  Widget total(DateTime lastDate, int runningTotal) {
     return Padding(
-      padding: const EdgeInsets.all(10),
-      child: ListView(
-        children: List<Widget>.generate(
-          ordersLen - 1,
-          (i) => Padding(
-            padding: const EdgeInsets.all(5),
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(width: 1.0, color: Colors.grey),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(5),
+      padding: const EdgeInsets.all(5),
+      child: Container(
+        decoration: const BoxDecoration(color: Colors.black12),
+        child: Padding(
+          padding: const EdgeInsets.all(5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(justTheDate(lastDate),
+                  style: const TextStyle(fontWeight: FontWeight.bold)),
+              const Text('合計', style: TextStyle(fontWeight: FontWeight.bold)),
+              SizedBox(
+                width: 80,
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    Text(
-                      formatTimestamp(
-                          a.rrOrderTimestamp(a.ctx, ordersLen - 2 - i)),
+                    const Icon(
+                      Icons.currency_yen,
+                      size: 10,
                     ),
-                    Text(
-                      paymentMethodJA(PaymentMethod.values[
-                          a.rrOrderPaymentMethod(a.ctx, ordersLen - 2 - i)]),
-                    ),
-                    SizedBox(
-                      width: 80,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          const Icon(
-                            Icons.currency_yen,
-                            size: 10,
-                          ),
-                          Text(a
-                              .rrOrderTotal(a.ctx, ordersLen - 2 - i)
-                              .toString()),
-                        ],
-                      ),
+                    SelectableText(
+                      runningTotal.toString(),
+                      style: const TextStyle(fontWeight: FontWeight.bold),
                     ),
                   ],
                 ),
               ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> entries() {
+    final a = widget.app;
+    var result = List<Widget>.empty(growable: true);
+    final ordersLen = a.rrOrdersLen(a.ctx);
+
+    DateTime? lastDate;
+    int runningTotal = 0;
+
+    for (var i = 0; i < ordersLen - 1; i += 1) {
+      final date = DateTime.fromMillisecondsSinceEpoch(
+          a.rrOrderTimestamp(a.ctx, ordersLen - 2 - i));
+
+      if (lastDate != null && lastDate.day != date.day) {
+        result.add(total(lastDate, runningTotal));
+        runningTotal = 0;
+      }
+
+      result.add(
+        Padding(
+          padding: const EdgeInsets.all(5),
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(width: 1.0, color: Colors.grey),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    formatTimestamp(a.rrOrderTimestamp(a.ctx, i)),
+                  ),
+                  Text(
+                    paymentMethodJA(
+                        PaymentMethod.values[a.rrOrderPaymentMethod(a.ctx, i)]),
+                  ),
+                  SizedBox(
+                    width: 80,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        const Icon(
+                          Icons.currency_yen,
+                          size: 10,
+                        ),
+                        SelectableText(a.rrOrderTotal(a.ctx, i).toString()),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
+      );
+      runningTotal += a.rrOrderTotal(a.ctx, i);
+
+      lastDate = date;
+    }
+
+    if (lastDate != null) {
+      result.add(total(lastDate, runningTotal));
+    }
+
+    return result.reversed.toList(growable: false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: ListView(
+        children: entries(),
       ),
     );
   }
